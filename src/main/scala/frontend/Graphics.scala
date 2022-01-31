56bba005a721a8820674bf9552aa5c5d24cf934a
AntoineGuilminCrepon <antoine.guilmin@ens-paris-saclay.fr>
2022-01-31 17:06:29 +0100

    Débilus

--- src/main/scala/frontend/Graphics.scala
+++ src/main/scala/frontend/Graphics.scala
@@ -11,9 +11,12 @@ class AttackButtons(b: Array[Button]) extends GridPanel(2, 2) {
 
 class Arena(allies: Array[TextField], enemies: Array[TextField]) extends GridPanel(2, 3) {
     preferredSize = new Dimension(1920, 800)
+
+    for (i <- 0 to 2) {
         contents += new Label{
             icon = new javax.swing.ImageIcon("src/main/resources/red_square.png")
         }
+    }
     for (i <- 0 to 2) {
         contents += allies(i)
     }
