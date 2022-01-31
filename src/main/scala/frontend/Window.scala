ba44eb545ffb3993bb287153f0076635c5dec885
pigeonmoelleux <balthazar.patiachvili@gmail.com>
2022-01-31 17:12:06 +0100

    Scission Graphics.scala

--- src/main/scala/frontend/Window.scala
+++ src/main/scala/frontend/Window.scala
@@ -1,26 +1,9 @@
-package graphics
+package window
 
 import swing._
 import swing.event._
 
-class AttackButtons(b: Array[Button]) extends GridPanel(2, 2) {
-    for (i <- 0 to 3) {
-        contents += b(i)
-    }
-}
-
-class Arena(allies: Array[TextField], enemies: Array[TextField]) extends GridPanel(2, 3) {
-    preferredSize = new Dimension(1920, 800)
-
-    for (i <- 0 to 2) {
-        contents += new Label{
-            icon = new javax.swing.ImageIcon("src/main/resources/red_square.png")
-        }
-    }
-    for (i <- 0 to 2) {
-        contents += allies(i)
-    }
-}
+import frames._
 
 object MyApp extends SimpleSwingApplication {
     val allies = new Array[TextField](3)
