fc20fa72a7978a09bbbf20e000f2c9f8097b9911
pigeonmoelleux <balthazar.patiachvili@gmail.com>
2022-02-01 15:13:00 +0100

    Test Monster.scala

--- src/main/scala/backend/Attack.scala
+++ src/main/scala/backend/Attack.scala
@@ -1,13 +1,20 @@
 package attack
 
-abstract class AttackType extends Enumeration {
-    type AttackType = Value
-    val RangeAttack, MeleeAttack = Value
+object AttackType {
+    sealed trait EnumVal
+    case object RangeAttack extends EnumVal
+    case object MeleeAttack extends EnumVal
 }
 
 abstract class Attack {
     val name : String
-    val attackType : AttackType
+    val attackType : AttackType.EnumVal
     
     val damageModifier : Int
+}
+
+class Punch extends Attack {
+    override val name = "Punch"
+    override val attackType : AttackType.EnumVal = AttackType.MeleeAttack
+    override val damageModifier = 1
 }
--- src/main/scala/backend/Fighter.scala
+++ src/main/scala/backend/Fighter.scala
@@ -12,6 +12,11 @@ abstract class Fighter {
     val strength : Int
     val toughness : Int
 
+    val attacks : Array[Attack]
+
+    /* Fonction retournant l'attaque choisie par le combattant */
+    def chooseAttack() : Attack
+
     /* Renvoie le nombre de dégats infligés par l'attaque */
     def fight(enemy : Fighter, attack : Attack) : Int = {
         val powerModifier = this.strength.toFloat / enemy.toughness
