1d5a5c7df6b90b8e4988663ef8ecaa9a7c863609
AntoineGuilminCrepon <antoine.guilmin@ens-paris-saclay.fr>
2022-02-04 18:02:37 +0100

    Tu vas pas me faire chier longtemps

--- src/main/scala/frontend/Frames.scala
+++ src/main/scala/frontend/Frames.scala
@@ -5,7 +5,6 @@ import scalafx.collections._
 import scalafx.scene.image._
 import scalafx.scene.layout._
 import scalafx.scene.control._
-<<<<<<< HEAD
 import scalafx.geometry._
 import scalafx.scene._
 
@@ -34,31 +33,3 @@ class AttackButtons extends GridPane {
 
     alignment = Pos.TopCenter
 }*/
-=======
-
-class AttackButtons(width: Int, height: Int) extends GridPane {
-
-    prefWidth = width
-    prefHeight = height
-
-    /*Ajout des boutons*/
-
-    for (i <- 1 to 4) {
-        val b = new Button(s"Attaque $i")
-        b.resize(width = 960, height = 200)
-        add(b, i%2, i/2)
-    }
-}
-
-class Arena(width: Int, height: Int) extends GridPane {
-
-    prefWidth = width
-    prefHeight = height
-
-    /*Ajout des images*/
-
-    for (i <- 0 to 5) {
-        add(new ImageView(new Image("red_square.png")), i%3, i/3)
-    }
-}
->>>>>>> 4862da4d70279418dbf55d2978c0642d07040b02
--- src/main/scala/frontend/Window.scala
+++ src/main/scala/frontend/Window.scala
@@ -10,23 +10,12 @@ import frames._
 
 object Battle extends JFXApp3 {
 
-<<<<<<< HEAD
     override def start () : Unit = {
         stage = new JFXApp3.PrimaryStage {
             title = "Fight Arena"
             scene = new Scene {
                 root = new StackPane {
                     children.add(new AttackButtons)
-=======
-
-    override def start () : Unit = {
-        stage = new JFXApp3.PrimaryStage {
-            title = "Fight Arena"
-            scene = new Scene(width = 1920, height =1080) {
-                root = new StackPane {
-                    children += new Arena(width = 1920, height = 680)
-                    children += new AttackButtons(width = 1920, height = 400)
->>>>>>> 4862da4d70279418dbf55d2978c0642d07040b02
                 }
             }
         }
