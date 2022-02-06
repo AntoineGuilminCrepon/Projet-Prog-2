837e12f6cc29ad67ec7c94511ee1021553d03a20
pigeonmoelleux <balthazar.patiachvili@gmail.com>
2022-02-06 12:49:06 +0100

    Réglages détails

--- src/main/scala/frontend/Frames.scala
+++ src/main/scala/frontend/Frames.scala
@@ -7,6 +7,8 @@ import scalafx.scene.layout._
 import scalafx.scene.control._
 import scalafx.geometry._
 import scalafx.scene._
+import scalafx.Includes._
+import scalafx.event.ActionEvent
 
 import hero._
 import monster._
@@ -28,6 +30,11 @@ class AttackButtons(avatar : Hero) extends GridPane {
         var b = new Button(avatar.attacks(i).toString())
         b.setMinWidth(w)
         b.setMinHeight(h)
+
+        b.onAction = handle {
+            println("Button pressed : " + i)
+        }
+
         add(b, i%2, i/2)
     }
 }
@@ -48,11 +55,11 @@ class Arena(allies : Array[Hero], enemies : Array[Monster]) extends GridPane {
 
     for (i <- 0 to 2) {
         var ivAllies = new ImageView(new Image(allies(i).visual, w, h, false, false))
-        add(ivAllies, (i%3)+1, i/3)
+        add(ivAllies, (i%3), 1)
 
         var ivEnemies = new ImageView(new Image(enemies(i).visual, w, h, false, false))
         ivEnemies.fitWidth = w
         ivEnemies.fitHeight = h
-        add(ivEnemies, (i%3)+1, i/3)
+        add(ivEnemies, (i%3), 0)
     }
 }
