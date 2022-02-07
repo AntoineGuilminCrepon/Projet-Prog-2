aeb433cd56695f300f8cd345c76ed154c8c1ef9a
AntoineGuilminCrepon <antoine.guilmin@ens-paris-saclay.fr>
2022-02-07 10:46:49 +0100

    Petit correctif

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,4 @@
-package main
+package window
 
 import scalafx._
 import scalafx.application._
@@ -6,18 +6,22 @@ import scalafx.scene._
 import scalafx.scene.layout._
 import scalafx.event._
 
-import frames._
+import arena._
+import attackmenu._
+import messagedisplay._
+import battle._
+import fighter._
 import hero._
 import monster._
 
-object Battle extends JFXApp3 {
+object FightArena extends JFXApp3 {
 
-    val allies = new Array[Hero](3)
-    val enemies = new Array[Monster](3)
+    val allies = new Array[Fighter](3)
+    val enemies = new Array[Fighter](3)
 
     for (i <- 0 to 2) {
-        allies(i) = new Swordman
-        enemies(i) = new Slime
+        allies(i) = new Swordman(i)
+        enemies(i) = new Slime(i+3)
     }
 
     override def start () : Unit = {
@@ -27,11 +31,13 @@ object Battle extends JFXApp3 {
             var battle = new Battle(messagesDispayer, allies, enemies)
             scene = new Scene(1290, 900) {
                 root = new StackPane {
-                    children.add(new AttackButtons(new Swordman))
-                    children.add(new Arena(allies, enemies))
+                    children.add(new AttackMenu(battle))
+                    children.add(messagesDispayer)
+                    children.add(new Arena(battle, allies, enemies))
                 }
             }
         }
         stage.setResizable(false)
+        stage.show()
     }
 }
